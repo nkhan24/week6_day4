f52b8a9181fa7163257d27c79ccc828900092a90
nkhan24 <naeem.m.khan18@gmail.com>
2022-05-19 17:54:19 +0300

    Created Controllers for Category, Constructor and Driver classes

--- SpringBootPjF1/src/main/java/com/example/SpringBootPjF1/controller/CategoryController.java
+++ SpringBootPjF1/src/main/java/com/example/SpringBootPjF1/controller/CategoryController.java
@@ -1,4 +1,30 @@
 package com.example.SpringBootPjF1.controller;
 
+import com.example.SpringBootPjF1.model.Category;
+import com.example.SpringBootPjF1.repository.CategoryRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
 public class CategoryController {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+    @GetMapping("/category")
+    public ResponseEntity<List<Category>> getAllCategory(){
+        List<Category> categories = categoryRepository.findAll();
+        return ResponseEntity.ok().body(categories);
+    }
+    @PostMapping("/category")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        Category result = categoryRepository.save(category);
+        return ResponseEntity.ok().body(result);
+    }
+
 }
--- SpringBootPjF1/src/main/java/com/example/SpringBootPjF1/controller/DriverController.java
+++ SpringBootPjF1/src/main/java/com/example/SpringBootPjF1/controller/DriverController.java
@@ -1,4 +1,39 @@
 package com.example.SpringBootPjF1.controller;
 
+
+import com.example.SpringBootPjF1.model.Constructor;
+import com.example.SpringBootPjF1.model.Driver;
+import com.example.SpringBootPjF1.repository.DriverRepository;
+import com.example.SpringBootPjF1.repository.DriverRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class DriverController {
+
+    private final DriverRepository driverRepository;
+
+    public DriverController(DriverRepository driverRepository){
+        this.driverRepository = driverRepository ;
+    }
+    @GetMapping("/constructors")
+    public ResponseEntity<List<Driver>> getAllCategory(){
+        List<Driver> drivers = driverRepository.findAll();
+        return ResponseEntity.ok().body(drivers);
+    }
+    @PostMapping("/constructors")
+    public ResponseEntity<Driver> createdriver(@RequestBody Driver driver){
+        Driver result = driverRepository.save(driver);
+        return ResponseEntity.ok().body(result);
+    }
+
+
+
+
+
 }
